a937d5bcfd59c219007b27bfaed9cf8fcaa90af5
sava911 <pesss929@gmail.com>
2023-03-28 05:39:54 +0300

    end gallery section slider

--- app/js/main.js
+++ app/js/main.js
@@ -19,4 +19,49 @@ $(function () {
     cssEase: 'linear'
   });
 
+  // SLIDER gallery
+  $(function () {
+    initSlider();
+  });
+
+
+  function initSlider() {
+
+    var slider = $(".gallery-slider");
+
+    slider.on("init", function (slick) {
+      checkSlides(this, 0);
+    });
+
+    slider.on("beforeChange", function (event, slick, currentSlide, nextSlide) {
+      $(this).addClass("changing");
+    });
+
+    slider.on("afterChange", function (event, slick, currentSlide) {
+      $(this).removeClass("changing");
+      checkSlides(this, currentSlide);
+    });
+
+
+    slider.slick({
+      prevArrow: "<button type='button' class='slick-prev' aria-label='Previous picture'></button>",
+      nextArrow: "<button type='button' class='slick-next' aria-label='Next Picture'></button>",
+      centerMode: true,
+      variableWidth: true,
+      dots: true,
+      // autoplay: true,
+      slidesToShow: 1,
+    });
+
+  }
+
+  function checkSlides(slider, currentSlide) {
+    var slides = $(".gallery-slide", $(slider));
+    slides.removeClass("prev");
+    slides.filter(function (index) {
+      return $(this).attr("data-slick-index") < currentSlide;
+    }).addClass("prev");;
+  }
+
+
 })
